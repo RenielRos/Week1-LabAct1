56065d0010a479d8bd5f60fa020432cde93f04f9
RenielRos <rosreniel2@gmail.com>
2024-09-19 13:17:58 +0900

    week1 | Laboratory Activity 1

---
+++ RosReniel_21-ITE-05.php
@@ -0,0 +1,49 @@
+<?php
+class Person {
+    public $firstName;
+    public $lastName;
+    private $age;
+
+    public function __construct($firstName, $lastName, $age) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->age = $age;
+    }
+
+    public function getFullName() {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+}
+
+class Car {
+    public $make;
+    public $model;
+    protected $year;
+
+    public function __construct($make, $model, $year) {
+        $this->make = $make;
+        $this->model = $model;
+        $this->year = $year;
+    }
+
+    public function getCarInfo() {
+        return $this->make . ' ' . $this->model . ' (' . $this->year . ')';
+    }
+}
+
+$person = new Person('Reniel', 'Ros', 20);
+echo 'Full Name: ' . $person->getFullName() . "<br>";
+echo  ' Age: ' . $person->getAge() . "<br>";
+$person->setAge(21);
+echo ' Updated Age: ' . $person->getAge() . "<br>";
+$car = new Car('Toyota', 'Camry', 2024);
+echo ' Car Info: ' . $car->getCarInfo();
+?>
